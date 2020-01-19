1356ef9c33b5587033302c042d7bad114b2f4856
shotaIDE <ide.shota.dev@gmail.com>
2020-01-19 21:30:46 +0900

    Add required information to publish on PyPI #4

--- setup.py
+++ setup.py
@@ -2,17 +2,35 @@
 
 from setuptools import setup
 
-PACKAGE_NAME = 'bracketdot'
-VERSION_NAME = '1.0'
+description = ('A tool that runs lint '
+               'focusing only on the latest changes in Git.')
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name=PACKAGE_NAME,
-    version=VERSION_NAME,
-    packages=[PACKAGE_NAME],
-    python_requires='>=3.6',
+    name='bracketdot',
+    version='1.0.0',
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/shotaIDE/BracketDot',
+    author='Shota Ide',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='development lint git',
+    packages=['bracketdot'],
     install_requires=[
         'pyspellchecker>=0.4.0',
     ],
+    python_requires='~=3.6',
     extras_require={
         'dev': [
             'pytest-pycodestyle',
