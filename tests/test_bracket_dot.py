e22d27a49a6831c39250c19996811c797b43c110
shota.ide <shota.ide@daj.co.jp>
2020-01-29 16:38:53 +0900

    Add test for the command to convert brakcets to dots #10

---
+++ tests/test_bracket_dot.py
@@ -0,0 +1,64 @@
+# coding: utf-8
+
+import pytest
+
+from bracketdot.bracket_dot import get_bracket_to_dot
+
+
+def test_not_convert():
+    line = ' textField.enabled = YES; '
+    result = get_bracket_to_dot(line=line)
+    assert result is None
+
+
+def test_method_call_without_arguments_when_return_value_not_used():
+    line = ' [view layoutIfNeeded]; '
+    result = get_bracket_to_dot(line=line)
+    assert result is None
+
+
+def test_method_call_without_arguments_when_return_value_used():
+    line = ' if ([view isFirstResponder]) {'
+    result = get_bracket_to_dot(line=line)
+    assert result == ' if (view.isFirstResponder) {'
+
+
+def test_getter_call():
+    line = ' UIColor *backgroundColor = [view backgroundColor]; '
+    result = get_bracket_to_dot(line=line)
+    assert result == ' UIColor *backgroundColor = view.backgroundColor; '
+
+
+def test_setter_call():
+    line = ' [view setBackgroundColor:backgroundColor]; '
+    result = get_bracket_to_dot(line=line)
+    assert result == ' view.backgroundColor = backgroundColor; '
+
+
+# def test_array_getter_call_with_int():
+#     line = ' NSString *test = [testArray objectAtIndex:2]; '
+#     result = get_bracket_to_dot(line=line)
+#     assert result == ' NSString *test = testArray[2]; '
+
+def test_array_getter_call_with_variable():
+    line = ' NSString *testValue = [testArray objectAtIndex:testIndex]; '
+    result = get_bracket_to_dot(line=line)
+    assert result == ' NSString *testValue = testArray[testIndex]; '
+
+
+def test_array_getter_call_with_variable():
+    line = ' [testArray replaceObjectAtIndex:testIndex withObject:testString]; '
+    result = get_bracket_to_dot(line=line)
+    assert result == ' testArray[testIndex] = testString; '
+
+
+def test_dictionary_getter_call_with_variale():
+    line = ' NSString *testValue = [testDictionary objectForKey:testKey]; '
+    result = get_bracket_to_dot(line=line)
+    assert result == ' NSString *testValue = testDictionary[testKey]; '
+
+
+def test_dictionary_setter_call_with_variable():
+    line = ' [testDictionary setObject:testValue forKey:testKey]; '
+    result = get_bracket_to_dot(line=line)
+    assert result == ' testDictionary[testKey] = testValue; '
