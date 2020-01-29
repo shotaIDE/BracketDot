d030b711d5eb8938903767525884052eac922dd8
shota.ide <shota.ide@daj.co.jp>
2020-01-29 17:36:29 +0900

    Fix tests to match function return type #10

--- tests/test_bracket_dot.py
+++ tests/test_bracket_dot.py
@@ -20,46 +20,47 @@ def test_method_call_without_arguments_when_return_value_not_used():
 def test_method_call_without_arguments_when_return_value_used():
     line = ' if ([view isFirstResponder]) {'
     result = get_bracket_to_dot(line=line)
-    assert result == ' if (view.isFirstResponder) {'
+    assert result['dst'] == ' if (view.isFirstResponder) {'
 
 
 def test_getter_call():
     line = ' UIColor *backgroundColor = [view backgroundColor]; '
     result = get_bracket_to_dot(line=line)
-    assert result == ' UIColor *backgroundColor = view.backgroundColor; '
+    assert result['dst'] == (' UIColor *backgroundColor = '
+                             'view.backgroundColor; ')
 
 
 def test_setter_call():
     line = ' [view setBackgroundColor:backgroundColor]; '
     result = get_bracket_to_dot(line=line)
-    assert result == ' view.backgroundColor = backgroundColor; '
+    assert result['dst'] == ' view.backgroundColor = backgroundColor; '
 
 
 # def test_array_getter_call_with_int():
 #     line = ' NSString *test = [testArray objectAtIndex:2]; '
 #     result = get_bracket_to_dot(line=line)
-#     assert result == ' NSString *test = testArray[2]; '
+#     assert result['dst'] == ' NSString *test = testArray[2]; '
 
 def test_array_getter_call_with_variable():
     line = ' NSString *testValue = [testArray objectAtIndex:testIndex]; '
     result = get_bracket_to_dot(line=line)
-    assert result == ' NSString *testValue = testArray[testIndex]; '
+    assert result['dst'] == ' NSString *testValue = testArray[testIndex]; '
 
 
 def test_array_getter_call_with_variable():
     line = (' [testArray replaceObjectAtIndex:testIndex '
             'withObject:testString]; ')
     result = get_bracket_to_dot(line=line)
-    assert result == ' testArray[testIndex] = testString; '
+    assert result['dst'] == ' testArray[testIndex] = testString; '
 
 
 def test_dictionary_getter_call_with_variale():
     line = ' NSString *testValue = [testDictionary objectForKey:testKey]; '
     result = get_bracket_to_dot(line=line)
-    assert result == ' NSString *testValue = testDictionary[testKey]; '
+    assert result['dst'] == ' NSString *testValue = testDictionary[testKey]; '
 
 
 def test_dictionary_setter_call_with_variable():
     line = ' [testDictionary setObject:testValue forKey:testKey]; '
     result = get_bracket_to_dot(line=line)
-    assert result == ' testDictionary[testKey] = testValue; '
+    assert result['dst'] == ' testDictionary[testKey] = testValue; '
